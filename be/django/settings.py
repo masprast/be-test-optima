70664d56c632b8d200b73a6a5db6a4bb7a1ecaf9
masprast <emailanyarepras@gmail.com>
2023-01-28 11:30:04 +0700

    docker env setup, but seems failed

---
+++ be/django/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysqlDB',
+        'USER': 'mysqlu',
+        'PASSWORD': 'mpassword',
+        'HOST': 'localhost',
+        'PORT': 6612,
+    }
+}
